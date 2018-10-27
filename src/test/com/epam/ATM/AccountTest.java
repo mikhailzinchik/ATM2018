5a2a2af9fe032d164b73e67e7967a464cce36537
Зинчик <misha-zin@mail.ru>
2018-10-27 12:42:02 +0300

    Default Changelist

---
+++ src/test/com/epam/ATM/AccountTest.java
@@ -0,0 +1,32 @@
+package com.epam.ATM;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class AccountTest {
+    private Account account;
+
+    @Test
+    public void testDeposit() {
+        assertEquals(-200, 200);
+        assertEquals(500, 500);
+        assertEquals(1, 1);
+
+    }
+
+    @Test
+    public void testWithDraw() {
+        assertEquals(-200, -200);
+        assertEquals(500, -500);
+        assertEquals(1, -1);
+    }
+
+    @Test
+    public void testGetBalance(){
+    }
+}
+
+
+
+
+
